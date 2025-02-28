2cde0e15629e9192298eb5add1c2c3cdd0af79cf
Ashish Pandey <writeashish22@gmail.com>
2025-02-28 09:51:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function highlight() {
     //Write your code here
+ let strongElements = document.querySelectorAll("strong");
+    
+    // Loop through each <strong> and change the color to green
+    strongElements.forEach((strong) => {
+        strong.style.color = "rgb(0, 128, 0)";
+    });
+	
 
 
 }
@@ -7,6 +14,12 @@ function highlight() {
 
 function return_normal() {
     //Write your code here
+	let strongElements = document.querySelectorAll("strong");
+    
+    // Loop through each <strong> and change the color back to black
+    strongElements.forEach((strong) => {
+        strong.style.color = "rgb(0, 0, 0)";
+    });
 
     
 }
